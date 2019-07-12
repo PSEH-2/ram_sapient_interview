81346f27c5a8d983210aaeabf3e6488990ca8edb
PSEH-5 <PSEH-5@sapient.com>
2019-07-12 13:58:13 +0530

    Updated initilizer

--- src/main/java/com/ram/weatherservice/RamWeatherServiceApplication.java
+++ src/main/java/com/ram/weatherservice/RamWeatherServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class RamWeatherServiceApplication {
+public class RamWeatherServiceApplication extends ServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RamWeatherServiceApplication.class, args);
